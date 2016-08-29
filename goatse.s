04bbe883d0ceb438cb9159f66b8c695ee20496a7
XlogicX <no.axiom@gmail.com>
2016-08-29 12:09:12 -0400

    Comments and Color
    -Added very verbose comments for those that want to study how the code works
    -Added color (0x0e / yellow text on black background), now using stosw instead of stosb due to that (and no longer having to inc di)
    -Modified the end of code and BIOS signature padding to be more flexible

--- goatse.s
+++ goatse.s
@@ -1,61 +1,65 @@
-bits 16
+bits 16                 ;16-bit mode
 org 0x7c00
 
 init:
     ; assumes bx=0
-    mov ax, 3
+    mov ax, 3           ;because some BIOS crash without this
     int 0x10
 
-    push bx
-    pop ds
-    push 0xb800
-    pop es
-    mov si, text
-    xor di, di
-    mov dx, lut
+    push bx             ;\
+    pop ds              ; \These two lines not always nessesary, but leaving them in
+    push 0xb800         ;text video memory
+    pop es              ;into es register
+    mov si, text        ;source index has pointer to goatse image
+    xor di, di          ;clear destination index
+    mov dx, lut         ;character set pointer to dx register
 
 iter:
-    lodsb
-    test al, al
-    jz endline
+    ;The main loop to print all of the characters
+    lodsb               ;get character from goatse image (and store in al register)
+    test al, al         ;is it null? (end of a line)
+    jz endline          ;if so, do the end-of-line routine
 
-    movzx cx, al
-    and cl, 0x0f
-    shr al, 0x04
-    test al, al
-    jz skipspace
+    movzx cx, al        ;goatse chars into cx
+    and cl, 0x0f        ;mask for the 2nd char (still in cl)
+    shr al, 0x04        ;shift for first char in al
+    test al, al         ;if there is no char
+    jz skipspace        ;then it is a command for how many spaces to skip
 
-    mov al, byte [edx+eax]
-    stosb
-    inc di
-    mov al, byte [edx+ecx]
-    stosb
-    inc di
-    jmp iter
+    mov al, byte [edx+eax]      ;character to use in lut.bin set, indexed with eax
+    mov ah, 0x0e                ;Yellow text on black background
+    stosw                       ;display the pixel
+    mov al, byte [edx+ecx]      ;;character to use in lut.bin set, indexed with ecx
+    stosw                       ;display the pixel
+    xor ax, ax                  ;clear a register
+    jmp iter                    ;repeat
 
 skipspace:
-    add di, cx
-    add di, cx
-    jmp iter
+    add di, cx                  ;increment video buffer by cx characters (if character was less than 16)
+    add di, cx                  ;^^^
+    jmp iter                    ;next character
 
 endline:
-    add bx, 80*2
-    mov di, bx
-    cmp bh, 0x0e
-    jnz iter
+    add bx, 80*2        ;amount of chars for a line
+    mov di, bx          ;put the next into the new di location
+    cmp bh, 0x0e        ;is it the last line (bx = 0x0e??)
+    jnz iter            ;if not, go to the next line
 
 done:
-    mov ah, 2
-    xor bh, bh
-    mov dx, 0x0b23
-    int 0x10
+    mov ah, 2           ;set cursor position
+    xor bh, bh          ;page number is 0
+    mov dx, 0x0b23      ;Row 0x0b, Column 0x23 (put cursor in the butthole)
+    int 0x10            ;call the BIOS function
 
 lut:
+    ;an index of characters used for display
     incbin "lut.bin"
 
 text:
+    ;If byte is less than 0x0f, skip that many spaces (if 0x0c, then skip 12 spaces)
+    ;otherwise, high nibble first, low nibble next; as an index into the lut.bin character list
     incbin "goatse.bin"
 
-times 446-($-$$) db 0
-times 64 db 0xff
-db 0x55, 0xaa
+;BIOS sig and padding
+times 510-($-$$) db 0
+dw 0xAA55
