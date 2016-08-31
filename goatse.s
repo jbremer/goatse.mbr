994e44a631d2448654c0b70e39914586080c49aa
XlogicX <no.axiom@gmail.com>
2016-08-31 17:57:02 -0400

    Made butthole red
    Made butthole red

--- goatse.s
+++ goatse.s
@@ -13,6 +13,14 @@ init:
     mov si, text        ;source index has pointer to goatse image
     xor di, di          ;clear destination index
     mov dx, lut         ;character set pointer to dx register
+    jmp iter
+
+nextimage:
+    mov si, text + 0x013d           ;location of next image in goatse.bin
+    mov bx, 0x0460                  ;location on screen to print anus
+    mov di, bx                      ;likewise
+    mov byte [color + 1], 0x04      ;change the color to red (self-modifying code)
+    mov word [lastline + 2], 0x0a00
 
 iter:
     ;The main loop to print all of the characters
@@ -27,7 +35,7 @@ iter:
     jz skipspace        ;then it is a command for how many spaces to skip
 
     mov al, byte [edx+eax]      ;character to use in lut.bin set, indexed with eax
-    mov ah, 0x0e                ;Yellow text on black background
+    color: mov ah, 0x0e                ;Yellow text on black background
     stosw                       ;display the pixel
     mov al, byte [edx+ecx]      ;;character to use in lut.bin set, indexed with ecx
     stosw                       ;display the pixel
@@ -42,10 +50,12 @@ skipspace:
 endline:
     add bx, 80*2        ;amount of chars for a line
     mov di, bx          ;put the next into the new di location
-    cmp bh, 0x0e        ;is it the last line (bx = 0x0e??)
-    jnz iter            ;if not, go to the next line
+    lastline: cmp bx, 0x0dc0      ;Is it the last line?
+    jb iter             ;if not, keep going
 
 done:
+    cmp bx, 0x0dc0      ;if it was the last line
+    je nextimage        ;go to the next image
     mov ah, 2           ;set cursor position
     xor bh, bh          ;page number is 0
     mov dx, 0x0b23      ;Row 0x0b, Column 0x23 (put cursor in the butthole)
